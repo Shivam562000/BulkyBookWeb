b3273c900923beb1a852baaefbbfcb1a6bcc8d4c
Shivam Chougule <Shivam.Chougule@lntinfotech.com>
2023-05-01 15:21:16 +0530

    created category model

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+    }
+}
+
+/*
+Code first approach  =>
+    Create models then use "entity framework" 
+
+DB first approach  =>
+    create database(tables) & use it code
+*/
+
